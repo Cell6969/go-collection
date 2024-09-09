6f3351391551496990b01ec1531c433875b55ee8
Cell6969 <bossmarinoo@gmail.com>
2024-09-09 14:41:01 +0700

    Add: type assertion

---
+++ basic/type_assertion.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func random() any {
+	return "Ok"
+}
+
+func main() {
+	var result any = random()
+	var resultString string = result.(string)
+	fmt.Println(resultString)
+
+	// Error karena return nya adalah string
+	//var resultInt int = result.(int)
+	//fmt.Println(resultInt)
+
+	// best handling
+	switch value := result.(type) {
+	case string:
+		fmt.Println("String", value)
+	case int:
+		fmt.Println("Int", value)
+	default:
+		fmt.Println("Unknown", value)
+	}
+}
