837643b400d9b4aeb99b78f8e4b4fa39c687ea8d
Cell6969 <bossmarinoo@gmail.com>
2024-09-11 14:05:49 +0700

    Add: open connection

--- database_go/database_test.go
+++ database_go/database_test.go
@@ -1,10 +1,21 @@
 package database_go
 
 import (
+	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"testing"
 )
 
 func TestEmpty(t *testing.T) {
 
+}
+
+func TestOpenConnection(t *testing.T) {
+	db, err := sql.Open("mysql", "root@tcp(localhost:33066)/test")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// TODO
 }
