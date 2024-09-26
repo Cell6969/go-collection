ec2870298d175f6214817e279230cf14a1186070
Cell6969 <bossmarinoo@gmail.com>
2024-09-26 11:17:35 +0700

    Add: test

--- restful api/main.go
+++ restful api/main.go
@@ -4,11 +4,9 @@ import (
 	"database/sql"
 	"github.com/go-playground/validator/v10"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"restful_api/app"
 	"restful_api/controller"
-	"restful_api/exception"
 	"restful_api/helper"
 	"restful_api/middleware"
 	"restful_api/repository"
@@ -21,17 +19,7 @@ func main() {
 	var categoryRepository repository.CategoryRepository = repository.NewCategoryRepository()
 	var categoryService service.CategoryService = service.NewCategoryService(categoryRepository, db, validate)
 	var categoryController controller.CategoryController = controller.NewCategoryController(categoryService)
-
-	var router *httprouter.Router = httprouter.New()
-
-	// set router
-	router.GET("/api/categories", categoryController.Find)
-	router.GET("/api/categories/:id", categoryController.FindById)
-	router.POST("/api/categories", categoryController.Create)
-	router.PUT("/api/categories/:id", categoryController.Update)
-	router.DELETE("/api/categories/:id", categoryController.Delete)
-
-	router.PanicHandler = exception.ErrorHandler
+	var router = app.NewRouter(categoryController)
 
 	var server http.Server = http.Server{
 		Addr:    ":3000",
